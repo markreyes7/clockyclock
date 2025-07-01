288e8a2e1da7d4222b724ce42f0ff1d6e0f4a683
Mark <mark.reyes7777@gmail.com>
2025-06-30 22:38:05 -0700

    added new connection endpoint. fixed minor issue with time not updating properly

--- sketch1.ino
+++ sketch1.ino
@@ -19,17 +19,15 @@
 
 // Include the RTC library
 #include "RTC.h"
-
 //Include the NTP library
 #include <NTPClient.h>
+#include "ArduinoGraphics.h"
 #include "Arduino_LED_Matrix.h"
-
 #if defined(ARDUINO_PORTENTA_C33)
 #include <WiFiC3.h>
 #elif defined(ARDUINO_UNOWIFIR4)
 #include <WiFiS3.h>
 #endif
-
 #include <WiFiUdp.h>
 #include "secrets.h"
 #include "ledmatrix.h" 
@@ -37,6 +35,8 @@
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
 char ssid[] = SECRET_SSID;        // your network SSID (name)
 char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
+char ssid2[]= SECRET_SSID2;
+char pass2[] = SECRET_PASS2;
 
 int wifiStatus = WL_IDLE_STATUS;
 WiFiUDP Udp; // A UDP instance to let us send and receive packets over UDP
@@ -83,6 +83,11 @@ void connectToWiFi(){
 
     // wait 10 seconds for connection:
     delay(10000);
+
+    Serial.println("Attempting to connect to second SSID:   ");
+    Serial.println(ssid);
+
+    wifiStatus = WiFi.begin(ssid2, pass2);
   }
 
   Serial.println("Connected to WiFi");
@@ -102,7 +107,7 @@ void setup(){
   // Get the current date and time from an NTP server and convert
   // it to UTC +2 by passing the time zone offset in hours.
   // You may change the time zone offset to your local one.
-  auto timeZoneOffsetHours = -7;
+  auto timeZoneOffsetHours = -8;
   auto unixTime = timeClient.getEpochTime() + (timeZoneOffsetHours * 3600);
   Serial.print("Unix time = ");
   Serial.println(unixTime);
@@ -116,8 +121,37 @@ void setup(){
 
 
   Serial.println("Initializing LED Matrix");
+  Serial.begin(115200);
+
   matrix.begin();
+
+
+  matrix.beginDraw();
+
+  matrix.stroke(0xFFFFFFFF);
+
+  // add some static text
+
+  // will only show "UNO" (not enough space on the display)
+
+  const char text[] = "UNO r4";
+
+  matrix.textFont(Font_4x6);
+
+  matrix.beginText(0, 1, 0xFFFFFF);
+
+  matrix.println(text);
+
+  matrix.endText();
+
+
+  matrix.endDraw();
+
+
+  delay(2000);
+
 }
+
 
 void loop(){
   RTCTime currentTime;
@@ -156,19 +190,39 @@ void loop(){
   Serial.println(currentTime.getSeconds());
 
 
-  delay(1000);
+  //delay(1000);
+  // Make it scroll!
 
-  const uint32_t* frame = getArtFrame("heart");
-  if (frame) matrix.loadFrame(frame);
-  delay(500);
+  matrix.beginDraw();
 
-  const uint32_t* frame2 = getArtFrame("happy");
-  if (frame2) matrix.loadFrame(frame2);
-  delay(500);
-  const uint32_t* frame3 = getArtFrame("one");
-  if (frame3) matrix.loadFrame(frame3);
-  delay(1500);
-  const uint32_t* frame4 = getArtFrame("testnum");
-  if (frame4) matrix.loadFrame(frame4);
-  delay(2500);
+
+  matrix.stroke(0xFF00FF00);
+
+  matrix.textScrollSpeed(180);
+
+  // add the textchar timeStr[6];
+  char timeStr[10];
+  sprintf(timeStr, "%02d:%02d", currentTime.getHour(), currentTime.getMinutes());
+  matrix.beginText(0, 1, 0xFF00FF00);
+
+  matrix.print(+ timeStr);
+  matrix.endText(SCROLL_LEFT);
+
+
+
+ 
+
+  // const uint32_t* frame = getArtFrame("heart");
+  // if (frame) matrix.loadFrame(frame);
+  // delay(500);
+
+  // const uint32_t* frame2 = getArtFrame("happy");
+  // if (frame2) matrix.loadFrame(frame2);
+  // delay(500);
+  // const uint32_t* frame3 = getArtFrame("one");
+  // if (frame3) matrix.loadFrame(frame3);
+  // delay(1500);
+  // const uint32_t* frame4 = getArtFrame("testnum");
+  // if (frame4) matrix.loadFrame(frame4);
+  // delay(2500);
 }

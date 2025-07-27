e5e2aea2981c75f8cfed7b42cfcd520d48c462d8
Mark Reyes <markreyes@Mac.lan>
2025-07-27 00:17:31 -0700

    added set time for timer logic. removed a bunch of whitespace and comments. done

--- sketch1.ino
+++ sketch1.ino
@@ -11,18 +11,15 @@
 #include "secrets.h"
 #include "ledmatrix.h" 
 
-
-
 /// different SSID's for my personal networks
 char ssid[] = SECRET_SSID;        
 char pass[] = SECRET_PASS;   
 char ssid2[]= SECRET_SSID2;
 char pass2[] = SECRET_PASS2;
 
-bool isTimerSet = false;  // mapped to a button that will set to true when clicked and call display timer. 
-
+bool isTimerSet = false;
 int wifiStatus = WL_IDLE_STATUS;
-WiFiUDP Udp; // A UDP instance to let us send and receive packets over UDP
+WiFiUDP Udp;
 NTPClient timeClient(Udp);
 WiFiServer server(80); 
 
@@ -34,16 +31,11 @@ const unsigned long interval = 1000;
 int countdownSeconds = 30 * 60;  // 30 minutes
 
 void printWifiStatus() {
-  
   Serial.print("SSID: ");
   Serial.println(WiFi.SSID());
-
-  // print your board's IP address:
   IPAddress ip = WiFi.localIP();
   Serial.print("IP Address: ");
   Serial.println(ip);
-
-  // print the received signal strength:
   long rssi = WiFi.RSSI();
   Serial.print("signal strength (RSSI):");
   Serial.print(rssi);
@@ -65,15 +57,10 @@ void connectToWiFi(){
     Serial.print("Attempting to connect to SSID: ");
     Serial.println(ssid);
     wifiStatus = WiFi.begin(ssid, pass);
-
-    // wait 10 seconds for connection:
     delay(15000);
-
     Serial.println("Attempting to connect to second SSID:   ");
     Serial.println(ssid2);
-
     wifiStatus = WiFi.begin(ssid2, pass2);
-    // wait 10 seconds for connection:
     delay(10000);
   }
 
@@ -83,35 +70,57 @@ void connectToWiFi(){
 
 void timerLoop() {
   updateCountdown();
-
   if (countdownSeconds > 0) {
     displayTimer();
   } else {
-    isTimerSet = false;  // stop timer when done
+    isTimerSet = false;
   }
 }
+
+void timerLogic(String request){
+  if (request.indexOf("GET /set-timer?minutes=") >= 0) {
+    int startIdx = request.indexOf("minutes=") + 8;
+    int endIdx = request.indexOf(" ", startIdx);
+    String numStr = request.substring(startIdx, endIdx);
+    int duration = numStr.toInt();
+    if (duration > 0) {
+      countdownSeconds = duration * 60;
+      previousMillis = millis();
+      isTimerSet = true;
+      Serial.print("Timer set for minutes: ");
+      Serial.println(duration);
+    }
+  }
+}
+
 void serverSetup(){
-  
   WiFiClient client = server.available();
 
   if (client) {
     Serial.println("New client connected");
     String currentLine = "";
+    String request = "";
 
     while (client.connected()) {
       if (client.available()) {
         char c = client.read();
+        request += c;
         Serial.write(c);
+
         if (c == '\n') {
           if (currentLine.length() == 0) {
-            // Send HTTP headers
+            timerLogic(request);
             client.println("HTTP/1.1 200 OK");
             client.println("Content-type:text/html");
             client.println();
             client.print("<p style=\"font-size:7vw;\">Click <a href=\"/B\">this</a> to set the timer.</p>");
             client.print("<p style=\"font-size:7vw;\">Click <a href=\"/O\">this</a> to turn off the timer.</p>");
+            client.print("<form action=\"/set-timer\" method=\"GET\">");
+            client.print("<label for=\"minutes\">Enter minutes:</label><br>");
+            client.print("<input type=\"number\" id=\"minutes\" name=\"minutes\" min=\"1\" max=\"999\" required><br><br>");
+            client.print("<input type=\"submit\" value=\"Start Timer\">");
+            client.print("</form>");
             client.println();
-
             break;
           } else {
             currentLine = "";
@@ -119,10 +128,10 @@ void serverSetup(){
         } else if (c != '\r') {
           currentLine += c;
         }
-        
+
         if (currentLine.endsWith("GET /B")){
           isTimerSet = true;
-          previousMillis = millis();  // reset timer
+          previousMillis = millis();
           countdownSeconds = 30 * 60;
           updateCountdown();
           displayTimer();
@@ -132,24 +141,17 @@ void serverSetup(){
         }
       }
     }
-
-    // Close connection
     client.stop();
     Serial.println("Client disconnected");
   }
 }
 
-
-
 void updateCountdown() {
-
   unsigned long currentMillis = millis();
   unsigned long elapsed = currentMillis - previousMillis;
 
   if (countdownSeconds > 0 && elapsed >= interval) {
-    Serial.println("THE FIRST SECONDS");
     int secondsPassed = elapsed / interval;
-    Serial.println(secondsPassed);
     countdownSeconds -= secondsPassed;
     previousMillis += secondsPassed * interval;
 
@@ -164,8 +166,6 @@ void updateCountdown() {
   }
 }
 
-
-
 void displayTimer() {
   char timeStr[10];
   int min = countdownSeconds / 60;
@@ -175,15 +175,11 @@ void displayTimer() {
   matrix.beginDraw();
   matrix.stroke(0xFF00FF00);
   matrix.textScrollSpeed(120);
-  matrix.beginText(0, 1, 0xFF00FF00); // x, y, color
+  matrix.beginText(0, 1, 0xFF00FF00);
   matrix.print(+ timeStr);
   matrix.endText(SCROLL_LEFT);
+  matrix.endDraw();
 }
-
-// void displayTimer(int timeToSet){}. get timeToSetFromUi BUTTON or something
-
-// 
-
 
 void setup(){
   Serial.begin(115200);
@@ -195,11 +191,8 @@ void setup(){
   timeClient.begin();
   timeClient.update();
 
-  server.begin(); //setup server for the timer
+  server.begin();
 
-  // Get the current date and time from an NTP server and convert
-  // it to UTC +2 by passing the time zone offset in hours.
-  // You may change the time zone offset to your local one.
   auto timeZoneOffsetHours = -7;
   auto unixTime = timeClient.getEpochTime() + (timeZoneOffsetHours * 3600);
   Serial.print("Unix time = ");
@@ -207,100 +200,51 @@ void setup(){
   RTCTime timeToSet = RTCTime(unixTime);
   RTC.setTime(timeToSet);
 
-  // Retrieve the date and time from the RTC and print them
   RTCTime currentTime;
   RTC.getTime(currentTime); 
   Serial.println("The RTC was just set to: " + String(currentTime));
 
-
   Serial.println("Initializing LED Matrix");
-  Serial.begin(115200);
-
   matrix.begin();
-
-
   matrix.beginDraw();
-
   matrix.stroke(0xFFFFFFFF);
-
-  // add some static text
-
-  // will only show "UNO" (not enough space on the display)
-
   const char text[] = "UNO r4";
-
   matrix.textFont(Font_4x6);
-
   matrix.beginText(0, 1, 0xFFFFFF);
-
   matrix.println(text);
-
   matrix.endText();
-
-
   matrix.endDraw();
-
-
   delay(2000);
-
 }
-
 
 void loop(){
-  serverSetup();  // Always check for incoming requests
-
-
-  //this will be fixed to check if timer mode has been activated. if so, cancel the connect and then do the timer. if not just do the timer.
+  serverSetup();
   RTCTime currentTime;
 
-  if (isTimerSet == true){
-    timerLoop();  // Run countdown + display timer
+  if (isTimerSet) {
+    timerLoop();
+  } else {
+    RTC.getTime(currentTime); 
+    Serial.print(currentTime.getDayOfMonth());
+    Serial.print("/");
+    Serial.print(Month2int(currentTime.getMonth()));
+    Serial.print("/");
+    Serial.print(currentTime.getYear());
+    Serial.print(" - ");
+    Serial.print(currentTime.getHour());
+    Serial.print(":");
+    Serial.print(currentTime.getMinutes());
+    Serial.print(":");
+    Serial.println(currentTime.getSeconds());
+
+    matrix.beginDraw();
+    matrix.stroke(0xFF00FF00);
+    matrix.textScrollSpeed(180);
+    char timeStr[10];
+    sprintf(timeStr, "%02d:%02d", currentTime.getHour(), currentTime.getMinutes());
+    matrix.beginText(0, 1, 0xFF00FF00);
+    matrix.print(+ timeStr);
+    matrix.endText(SCROLL_LEFT);
+    matrix.endDraw();
   }
-  else {
-  RTC.getTime(currentTime); 
-
-  // Print out date (DD/MM//YYYY)
-  Serial.print(currentTime.getDayOfMonth());
-  Serial.print("/");
-  Serial.print(Month2int(currentTime.getMonth()));
-  Serial.print("/");
-  Serial.print(currentTime.getYear());
-  Serial.print(" - ");
-
-  // Print time (HH/MM/SS)
-  Serial.print(currentTime.getHour());
-  Serial.print(":");
-  Serial.print(currentTime.getMinutes());
-  Serial.print(":");
-  Serial.println(currentTime.getSeconds());
-
-  matrix.beginDraw();
-  matrix.stroke(0xFF00FF00);
-  matrix.textScrollSpeed(180);
-
-  char timeStr[10];
-  sprintf(timeStr, "%02d:%02d", currentTime.getHour(), currentTime.getMinutes());
-
-  matrix.beginText(0, 1, 0xFF00FF00);
-  matrix.print(+ timeStr);
-  matrix.endText(SCROLL_LEFT);
-  matrix.endDraw();
 }
-
-
- 
-
-  // const uint32_t* frame = getArtFrame("heart");
-  // if (frame) matrix.loadFrame(frame);
-  // delay(500);
-
-  // const uint32_t* frame2 = getArtFrame("happy");
-  // if (frame2) matrix.loadFrame(frame2);
-  // delay(500);
-  // const uint32_t* frame3 = getArtFrame("one");
-  // if (frame3) matrix.loadFrame(frame3);
-  // delay(1500);
-  // const uint32_t* frame4 = getArtFrame("testnum");
-  // if (frame4) matrix.loadFrame(frame4);
-  // delay(2500);
-
